f6fe2d3a5d33b4161bda04c5ce07a3c1fc95d701
Tonghui Li <li@indeed.com>
2015-12-13 01:24:57 +0900

    iOS v1

---
+++ PropertyView.js
@@ -0,0 +1,69 @@
+'use strict';
+ 
+var React = require('react-native');
+var {
+  StyleSheet,
+  Image, 
+  View,
+  Text,
+  Component
+} = React;
+
+class PropertyView extends Component {
+ 
+  render() {
+    var property = this.props.property;
+    var price = property.price_formatted.split(' ')[0];
+    return (
+      <View style={styles.container}>
+        <Image style={styles.image} 
+            source={{uri: property.img_url}} />
+        <View style={styles.heading}>
+          <Text style={styles.price}>£{price}</Text>
+          <Text style={styles.title}>{property.title}</Text>
+          <View style={styles.separator}/>
+        </View>
+        <Text style={styles.description}>Type: {property.property_type}</Text>
+        <Text style={styles.description}>Bedrooms: {property.bedroom_number}</Text>
+        <Text style={styles.description}>bathrooms: {property.bathroom_number}</Text>
+        <Text style={styles.description}>{property.summary}</Text>
+      </View>
+    );
+  }
+
+}
+
+var styles = StyleSheet.create({
+  container: {
+    marginTop: 65
+  },
+  heading: {
+    backgroundColor: '#F8F8F8',
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#DDDDDD'
+  },
+  image: {
+    width: 400,
+    height: 300
+  },
+  price: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    margin: 5,
+    color: '#48BBEC'
+  },
+  title: {
+    fontSize: 20,
+    margin: 5,
+    color: '#656565'
+  },
+  description: {
+    fontSize: 18,
+    margin: 5,
+    color: '#656565'
+  }
+});
+
+module.exports = PropertyView;
